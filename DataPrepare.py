b79415ece9acc380e1f48dea6ecf982bc9b0cd06
KuroiZZ <habiltatar01@gmail.com>
2026-04-23 15:28:38 +0300

    Created a new dataset with drop duplicates from cleaned column dataset.

--- DataPrepare.py
+++ DataPrepare.py
@@ -6,3 +6,8 @@ def select_columns():
     df = df.drop(columns=["Ratings", "Resenhas","genres","Description"])
     df.rename(columns={df.columns[0]: "Index"}, inplace=True)
     df.to_csv("DataSets/MovieReviews_CleanedColumns.csv", index=False)
+
+def drop_duplicates():
+    df = pd.read_csv("DataSets/MovieReviews_CleanedColumns.csv")
+    df = df.drop_duplicates(subset=["Reviews"])
+    df.to_csv("DataSets/MovieReviews_NoDuplicates.csv", index=False)
--- main.py
+++ main.py
@@ -1,3 +1,4 @@
 import DataPrepare as dpre
 
-dpre.select_columns()
+#dpre.select_columns()
+dpre.drop_duplicates()
